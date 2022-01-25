6f88eadeb2b444ff81e44106f47976869a9e5b2b
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 23:58:04 +0100

    Very first brownffin

---
+++ src/main/java/com/jteam/brownffins/setup/Registration.java
@@ -0,0 +1,32 @@
+package com.jteam.brownffins.setup;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static com.jteam.brownffins.Brownffins.MODID;
+
+public class Registration {
+
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+
+    public static void init() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        BLOCKS.register(bus);
+        ITEMS.register(bus);
+    }
+
+    public static final Item.Properties ITEM_PROPERTIES = new Item.Properties().tab(ModSetup.TAB_GROUP);
+
+
+    public static final RegistryObject<Item> BROWNFFIN = ITEMS.register("Brownffin",() -> new Item(ITEM_PROPERTIES));
+
+
+
+}
